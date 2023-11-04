61ff6950e61d2186b4118a6bf03f51d36fa8ed47
shwetadatey <147021044+shwetadatey@users.noreply.github.com>
2023-11-04 18:56:37 +0530

    Add files via upload

---
+++ Addition.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    clrscr();
+    int a,b,c,d;
+    printf("Enter three numbers:\n");
+    scanf("%d %d %d",&a,&b,&c);
+    d=a+b+c;
+    printf("Addition of numbers is= %d\n",d);
+    getch();
+}
---
+++ sub.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter the numbers:");
+    scanf("%d%d",&a,&b);
+    c=a-b;
+    printf("Subtraction is=%d",c);
+    getch();
+    }
+
+
+
+
+
+
+
+
+
+
